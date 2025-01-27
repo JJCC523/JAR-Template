e46599a6b13b18a58bfa0e86711d1fcc68d40828
Jack Cooper <105135311+JJCC523@users.noreply.github.com>
2025-01-27 14:37:26 -0500

    Update autons.cpp

--- src/autons.cpp
+++ src/autons.cpp
@@ -10,14 +10,14 @@
 
 void default_constants(){
   // Each constant set is in the form of (maxVoltage, kP, kI, kD, startI).
-  chassis.set_drive_constants(10, 1.5, 0, 10, 0);
+  chassis.set_drive_constants(12, 1.4, 0, 0, 0);
   chassis.set_heading_constants(6, .4, 0, 1, 0);
-  chassis.set_turn_constants(12, .4, .03, 3, 15);
+  chassis.set_turn_constants(12, .4, 0, 0, 15);
   chassis.set_swing_constants(12, .3, .001, 2, 15);
 
   // Each exit condition set is in the form of (settle_error, settle_time, timeout).
   chassis.set_drive_exit_conditions(1.5, 300, 5000);
-  chassis.set_turn_exit_conditions(1, 300, 3000);
+  chassis.set_turn_exit_conditions(1, 300, 1500);
   chassis.set_swing_exit_conditions(1, 300, 3000);
 }
 
@@ -39,6 +39,14 @@ void odom_constants(){
 /**
  * The expected behavior is to return to the start position.
  */
+
+void tunePID(){
+  chassis.drive_distance(24)
+  chassis.turn_to_angle(90);
+  Intake.spin(fwd, 100, pct);
+}
+
+
 
 void drive_test(){
   chassis.drive_distance(6);
@@ -98,6 +106,13 @@ void odom_test(){
     Brain.Screen.printAt(5,100, "SidewaysTracker: %f", chassis.get_SidewaysTracker_position());
     task::sleep(20);
   }
+}
+
+void tuneOdom(){
+  thread o(odom_test);
+  chassis.drive_to_point(24, 25);
+  Intake.spin(fwd, 100, pct);
+  
 }
 
 /**
