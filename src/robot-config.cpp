5ae453aceac457b6a609c60b850ba72a8b8dcd93
Jack Cooper <105135311+JJCC523@users.noreply.github.com>
2025-01-27 14:08:35 -0500

    Update robot-config.cpp

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -6,7 +6,16 @@ using code = vision::code;
 
 // A global instance of brain used for printing to the V5 Brain screen.
 brain  Brain;
+motor l1 = motor(PORT1, true);
+motor l2 = motor(PORT1, false);
+motor l3 = motor(PORT1, true);
+motor r1 = motor(PORT1, true);
+motor r2 = motor(PORT1, false);
+motor r3 = motor(PORT1, false);
 
+motor intake = motor(PORT2, false);
+
+rotation vert = rotation(PORT4, true);
 //The motor constructor takes motors as (port, ratio, reversed), so for example
 //motor LeftFront = motor(PORT1, ratio6_1, false);
 
