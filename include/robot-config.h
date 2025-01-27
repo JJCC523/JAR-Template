449aee06d82594c53650ee5f49ff2d6869541ffd
Jack Cooper <105135311+JJCC523@users.noreply.github.com>
2025-01-27 14:10:22 -0500

    Update robot-config.h

--- include/robot-config.h
+++ include/robot-config.h
@@ -4,7 +4,16 @@ extern brain Brain;
 
 //To set up a motor called LeftFront here, you'd use
 //extern motor LeftFront;
+extern motor l1;
+extern motor l2;
+extern motor l3;
+extern motor r1;
+extern motor r2;
+extern motor r3;
 
+extern motor intake;
+
+extern rotation vert;
 //Add your devices below, and don't forget to do the same in robot-config.cpp:
 
 
